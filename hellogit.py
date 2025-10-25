29c4882eb184942c6d6d6e918b8c2c33cade92d8
Diego <llanesdiego5@gmail.com>
2025-10-25 05:49:07 -0700

    terminal testing

--- hellogit.py
+++ hellogit.py
@@ -1,3 +1,4 @@
 print("New Hello Git")
 print("Me resingo en la git")
 # Para poner en talla la terminal :https://ohmyz.sh
+print("Agregando un detalle para comprobar que la terminal me lo pase.ijijij")
